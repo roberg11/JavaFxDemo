59e9bca05e11ec2873948de5bee68cbc2792f05f
roberg11 <r.gjengaar@gmail.com>
2016-04-20 14:31:00 +0200

    Revert "Revert "JavaFX starting files""
    This reverts commit f3827e79ce4c130989c5490ff95a75cb4cdfb337.

---
+++ src/embeddingLayouts/ConfirmBox.java
@@ -0,0 +1,53 @@
+package embeddingLayouts;
+
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+
+public class ConfirmBox {
+	
+	static boolean answer;
+	
+	public static boolean display(String title, String message){
+		Stage window = new Stage();
+		window.initModality(Modality.APPLICATION_MODAL);
+		window.setTitle(title);
+		window.setMinWidth(250);
+		
+		// Label stuff
+		Label label = new Label();
+		label.setText(message);
+		
+		// button stuff
+		Button yesButton = new Button("yes");
+		Button noButton = new Button("no");
+			
+		yesButton.setOnAction(e -> {
+			answer = true;
+			window.close();
+		});
+				
+		noButton.setOnAction(e -> {
+			answer = false;
+			window.close();
+		});
+		
+		// layout stuff
+		VBox layout = new VBox(10);
+		layout.getChildren().addAll(label, yesButton, noButton);
+		layout.setAlignment(Pos.CENTER);
+		
+		
+		// Scene and window stuff
+		Scene scene = new Scene(layout);
+		window.setScene(scene);
+		// showAndWait() shows the window and needs to be closed
+		// before going back to the main window
+		window.showAndWait();
+		
+		return answer;
+	}
+
+}
